e69a9d50e3f3280d192c30094d1e8ea24fdc22c1
Nikx1229 <89346251+JhakeMC@users.noreply.github.com>
2022-05-03 15:51:49 +0800

    Update musics.py

--- musics.py
+++ musics.py
@@ -1,8 +1,7 @@
-import python-mpv
+import os
 import random
 
 num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43]
+basCommand = f"mpv https://www.youtube.com/watch?v=61VB6yS7RB8&list=PL8UbYpPIoqN3N7u4pVJkI30V6Rb0zvDmO&index={random.choice(num)}"
 
-player = mpv.MPV(ytdl=True)
-player.play(f'https://www.youtube.com/watch?v=61VB6yS7RB8&list=PL8UbYpPIoqN3N7u4pVJkI30V6Rb0zvDmO&index={random.choice(num)}')
-player.wait_for_playback()
+os.system(bashCommand)
